e107c0167b32c1819a46b26f733982570caa621a
Sean Bergam <sbergam@elementum.com>
2026-09-10 13:10:34 -0600

    timeline: ONTO-13-ENG + ONTO-1 pilots

--- app/timelines/[id]/page.tsx
+++ app/timelines/[id]/page.tsx
@@ -3,6 +3,8 @@ import TimelineView from "./TimelineView";
 export function generateStaticParams() {
   return [
     { id: 'example' },
+    { id: '3d75dbdd-f0ea-8145-9a95-f418353a05b5' },
+    { id: '3d65dbdd-f0ea-81f6-880f-e041eda42d5b' },
   ];
 }
 
